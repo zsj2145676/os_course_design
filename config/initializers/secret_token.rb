da017588659eb9bbae8e67b6342f23d09dca7845
Sergio Zhao <zsj.jacob@gmail.com>
2013-06-24 12:03:29 +0800

    new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OsCourseDesign::Application.config.secret_key_base = '6232debb10b0f08eb472675e35339a652c7d5a6154b095a6abc0892384ee305caa8a780583cee5e58a56d9ee13ff317ee3ba7faddb288883d15338e49e954153'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OsCourseDesign::Application.config.session_store :cookie_store, key: '_os_course_design_session'
